2b506a9f64df41ba8cead2b96a5d2d02909df4f7
Manoel Rocha <manoelangelo@gmail.com>
2021-03-31 19:29:29 -0300

    primeira pagina

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
+import Image from 'next/image'
+
 function Home(){
-    return <div>EM MANUTENCAO</div>
+    
+    return <div>
+        <br></br>
+        <br></br>
+        <div class="card mb-4" align="center">
+            <h1>**** EM MANUTENCAO ****</h1>  
+            <h3>Contato: (21) 98600-5250</h3>  
+            <img></img>
+            <image src="pg_manut.jpg" width="300" height="200"></image>
+        </div>
+        
+        <title>Segmatech Soluções em Informática</title>
+
+    </div>
     
 }
 export default Home
+
